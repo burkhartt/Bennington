79645edc0b726e3828aac20a77c4e616d953f201
Tim Burkhart <burkhartt@digitalev.com>
2012-03-29 14:25:04 -0500

    Added a logout button

---
+++ src/Bennington.Cms/IconMenuRegistration.cs
@@ -0,0 +1,21 @@
+﻿using System.Web;
+using Bennington.Cms.MenuSystem;
+
+namespace Bennington.Cms
+{
+    public class MenuSystemConfigurer : IMenuSystemConfigurer
+    {
+        public void Configure(IMenuRegistry sectionMenuRegistry)
+        {
+            sectionMenuRegistry.Add(new UrlIconMenuItem("Sign Out",
+                                                        HttpContext.Current.Request.Url.AbsoluteUri +
+                                                        QuestionMarkOrAmpersand() + "Logout=Logout",
+                                                        "~/Content/Canvas/lock.png"));
+        }
+
+        private static string QuestionMarkOrAmpersand()
+        {
+            return HttpContext.Current.Request.Url.AbsoluteUri.Contains("?") ? "&" : "?";
+        }
+    }    
+}
